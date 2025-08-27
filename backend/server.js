36d7bf3f65bdc698e9d961887b42632d396891d2
saengtavanh <85266620+saengtavanh@users.noreply.github.com>
2025-08-27 16:40:44 +0700

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/send-sms", async (req, res) => {
+    try {
+        const { recipient, text } = req.body;
+        const response = await axios.post(`https://api.textmebot.com/send.php?recipient=${recipient}&apikey=ahLZYn4bFppP&text=${text}`, {
+            headers: {
+                "Accept": "application/json", 
+                "Content-Type": "application/json"
+            }});
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to send message" });
+    }
+});
+app.listen(5000, () => console.log("Server running on port 5000"));
